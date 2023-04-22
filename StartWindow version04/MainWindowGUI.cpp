624fadc184730275284d017c3ec827bfaf61883f
Raymin <112410546+RayminQAQ@users.noreply.github.com>
2023-04-22 22:39:04 +0800

    Add files via upload

---
+++ StartWindow version04/MainWindowGUI.cpp
@@ -0,0 +1,26 @@
+#include "MainWindowGUI.h"
+#include "RestartWindow.h"
+
+#include <iostream>
+MainWindowGUI::MainWindowGUI(QWidget* parent) 
+	: QWidget(parent)
+{
+	QVBoxLayout* vLayout = new QVBoxLayout(this);
+
+	RestartWindow* re = new RestartWindow;
+	connect(re, &RestartWindow::signal_Windows_Game_restart, this, &MainWindowGUI::open_StartWindow);
+
+	vLayout->addWidget(re);
+	
+}
+
+// TO DO: StartWindow
+void MainWindowGUI::open_StartWindow() {
+	qDebug() << "Open";
+}
+
+
+
+
+
+
